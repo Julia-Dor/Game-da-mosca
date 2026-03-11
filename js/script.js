56d444574e315dd1b85d39cdb05b4b240c2e84c8
Julia_Dornelles <juliadornelles23@gmail.com>
2026-03-11 16:52:06 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,23 +3,61 @@ const tabuleiro = document.querySelector("#tabuleiro")
 const mosca = document.querySelector("#mosca")
 const tempo = document.querySelector("#tempo")
 const acertos = document.querySelector("#acertos")
+const btnIniciar = document.querySelector("#btn-iniciar")
+const telaInicial = document.querySelector("aside")
+const telaJogo = document.querySelector("main")
+const gameOver = document.querySelector("#game-over")
 const root = document.documentElement
 
 //Variáveis de início
-var qtAcertos = 0, minutos = 2, segundos = 0, min = 5, max = 95, velocidade = 500 
+var qtAcertos = 0, minutos = 2, segundos = 0, min = 5, max = 95, velocidade = 600, movimento, temporizador
+
+//Temporizador
+function timer() {
+    segundos--
+    if( segundos == -1 ) {
+        minutos--
+        segundos = 59
+    }
+
+    tempo.innerHTML = (segundos < 10)
+            ? minutos + ":0" + segundos
+            : minutos + ":" + segundos
+}
 
 //Evento de clique sobre a mosca
 mosca.addEventListener("click", ()=>{
+    mosca.style.pointerEvents = "none"
+    mosca.src = "../img/moscaEsmagada.png"
+
+    setTimeout(()=>{
+        mosca.style.pointerEvents = "all"
+        mosca.src = "../img/mosca.png"
+    }, 1500)
+
     qtAcertos++
     acertos.innerHTML = qtAcertos
 
-    velocidade-=10
+    velocidade-=20
+    clearInterval(movimento)
+    movimento = setInterval(moveMosca, velocidade)
 
     root.style.setProperty("--velocidade", velocidade/1000 + "s")
 })
 
-//Temporizador para movimento da mosca
-setInterval(()=>{
+// evento para iniciar o jogo 
+btnIniciar.addEventListener("click", ()=> {
+    telaInicial.style.display = "none"
+    telaJogo.style.display = "block"
+    //Temporizador para movimento da mosca
+    movimento = setInterval(moveMosca, velocidade);
+
+    //Inicio do timer
+    temporizador = setInterval(timer, 1000)
+})
+
+
+function moveMosca() {
     let vertical = Math.floor (Math.random() * (max - min + 1)) + min
     let horizontal = Math.floor(Math.random() * (max - min + 1 )) + min
 
@@ -29,5 +67,4 @@ setInterval(()=>{
     console.log("Y = " + vertical)
     console.log("X = " + horizontal)
     console.log("Velocidade = " + velocidade)
-
-}, velocidade);
+}
